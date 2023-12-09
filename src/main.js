cc666567f34bb7c653efc8db68bf522b972d62ac
KazuulWork <atomnerobat@gmail.com>
2023-12-09 18:07:34 +0300

    переехал

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import { render } from './render';
+import ListFilterView from './view/filters';
+import ListSortingView from './view/sorting';
+import creatingEventView from './view/creating-form';
+import EventView from './view/route-point';
+
+
+const tripHeaderNode = document.querySelector('.trip-main');
+const filterContainerNode = tripHeaderNode.querySelector(
+  '.trip-controls__filters'
+);
+
+const tripMainNode = document.querySelector('.trip-events');
+
+render(new ListFilterView(), filterContainerNode);
+render(new ListSortingView(), tripMainNode);
+render(new creatingEventView(), tripMainNode);
+render(new EventView(), tripMainNode);
+render(new EventView(), tripMainNode);
+render(new EventView(), tripMainNode);
+
+//засунуть все в список, сделать всё пунктами списка
